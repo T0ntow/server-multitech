609b18594f9ddc694cf2b951783ffde960113b8f
Wellington Batista <wellingtonbs109@gmail.com>
2023-11-08 22:45:47 -0300

    update connection

--- src/connection.js
+++ src/connection.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 // Criando o pool de conexões
 const connection = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'WPP102315tom@',
-  database: 'multitech',
+  host: 'b455bmcwcsgk9mzdmv42-mysql.services.clever-cloud.com',
+  user: 'uhkuir4ghr3cuv9v',
+  password: 'LP6OloKoUFkR3eoVlpvW',
+  database: 'b455bmcwcsgk9mzdmv42',
 });
 
 // Obtendo uma conexão do pool
